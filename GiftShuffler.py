b15df3a036a2b835c9196f242baf37a7d06cd02b
Aggraxis <47491933+Aggraxis@users.noreply.github.com>
2021-11-25 10:53:01 -0600

    wrapped everything in a __main__ block.

--- GiftShuffler.py
+++ GiftShuffler.py
@@ -2,27 +2,28 @@
 import random
 import csv
 
-fileName="participants.csv"
-participantList = []
-with open (fileName) as csvfile:
-    reader = csv.DictReader(csvfile)
-    participantList = list(reader)
-recipientList = participantList.copy()
-givingDict = {}
-for participant in participantList:
-    #this is most likely not pythonic.
-    found = False
-    while not(found):
-        potentialRecipient = random.choice(recipientList)
-        if participant['NAME'] != potentialRecipient['NAME']:
-            givingDict[participant['NAME']] = potentialRecipient['NAME']
-            recipientList.remove(potentialRecipient)
-            found = True
-for key in givingDict:
-    outFileName="send_to_" + key + ".txt"
-    text_file = open(outFileName, "wt")
-    n = text_file.write(f'{key} gives to {givingDict[key]}\n')
-    text_file.close()
+if __name__ == "__main__":
+    fileName="participants.csv"
+    participantList = []
+    with open (fileName) as csvfile:
+        reader = csv.DictReader(csvfile)
+        participantList = list(reader)
+    recipientList = participantList.copy()
+    givingDict = {}
+    for participant in participantList:
+        #this is most likely not pythonic.
+        found = False
+        while not(found):
+            potentialRecipient = random.choice(recipientList)
+            if participant['NAME'] != potentialRecipient['NAME']:
+                givingDict[participant['NAME']] = potentialRecipient['NAME']
+                recipientList.remove(potentialRecipient)
+                found = True
+    for key in givingDict:
+        outFileName="send_to_" + key + ".txt"
+        text_file = open(outFileName, "wt")
+        n = text_file.write(f'{key} gives to {givingDict[key]}\n')
+        text_file.close()
 
 
 
