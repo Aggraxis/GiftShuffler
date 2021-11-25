db698fde962f429734a30d796e599d445ba7d86c
Aggraxis <47491933+Aggraxis@users.noreply.github.com>
2021-11-25 00:12:12 -0600

    Initial commit
    It...works?! It WORKS! Woooooooo!

---
+++ GiftShuffler.py
@@ -0,0 +1,28 @@
+#import os
+import random
+import csv
+
+fileName="participants.csv"
+participantList = []
+with open (fileName) as csvfile:
+    reader = csv.DictReader(csvfile)
+    participantList = list(reader)
+recipientList = participantList.copy()
+givingDict = {}
+for participant in participantList:
+    #this is most likely not pythonic.
+    found = False
+    while not(found):
+        potentialRecipient = random.choice(recipientList)
+        if participant['NAME'] != potentialRecipient['NAME']:
+            givingDict[participant['NAME']] = potentialRecipient['NAME']
+            recipientList.remove(potentialRecipient)
+            found = True
+for key in givingDict:
+    outFileName="send_to_" + key + ".txt"
+    text_file = open(outFileName, "wt")
+    n = text_file.write(f'{key} gives to {givingDict[key]}\n')
+    text_file.close()
+
+
+
